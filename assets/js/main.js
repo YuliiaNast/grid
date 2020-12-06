08a5008c7f11778eec4b303ce3050f77bf18b67b
YuliiaNast <luchachenko@gmail.com>
2020-12-06 18:51:20 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('header-navbar__btn--active');
+		$('.header-navbar__list').slideToggle(500) ;
+	});
+});
